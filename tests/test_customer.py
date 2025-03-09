977a656504bea23d21317176c109fa4939d61136
joshuaharris07 <joshua.harris07@gmail.com>
2025-03-08 19:11:07 -0700

    Added login route tests.

--- tests/test_customer.py
+++ tests/test_customer.py
@@ -1,5 +1,6 @@
 from app import create_app
 from app.models import db, Customer
+from datetime import datetime
 import unittest
 
 class TestCustomer(unittest.TestCase):
@@ -21,6 +22,30 @@ class TestCustomer(unittest.TestCase):
 
         self.client = self.app.test_client()
     
+
+    def test_login_cutomer(self):
+        credentials = {
+            "email": "jane@email.com",
+            "password": "1234"
+        }
+
+        response = self.client.post('/customers/login', json=credentials)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json['status'], 'success')
+        response.json['token']
+        return response.json['token']
+    
+
+    def test_invalid_login(self):
+        credentials = {
+            "email": "bad_email@email.com",
+            "password": "bad_pw"
+        }
+
+        response = self.client.post('/customers/login', json=credentials)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json['message'], 'Invalid email or password')
+
     def test_create_customer(self):
         customer_payload = {
         "name": "John Doe",
@@ -32,6 +57,17 @@ class TestCustomer(unittest.TestCase):
         response = self.client.post('/customers/', json=customer_payload)
         self.assertEqual(response.status_code, 201)
         self.assertEqual(response.json['name'], "John Doe")
+    
+    def test_invalid_creation(self):
+        customer_payload = {
+            "name": "John Doe",
+            "phone": "123-456-7890",
+            "password": "123"       
+        }
+
+        response = self.client.post('/customers/', json=customer_payload)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json['email'], ['Missing data for required field.'])
     
     def test_get_customers(self):
         response = self.client.get('/customers/')
@@ -49,3 +85,5 @@ class TestCustomer(unittest.TestCase):
     #     response = self.client.put('/customers/', json=customer_payload)
     #     self.assertEqual(response.status_code, 201)
     #     self.assertEqual(response.json[0]['name'], "Johnathan Doe")
+
+# python -m unittest discover tests
