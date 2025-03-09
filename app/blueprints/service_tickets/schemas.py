abbe662a7c1f2b801bfc2cea352774c67760f188
joshuaharris07 <joshua.harris07@gmail.com>
2025-03-08 22:23:19 -0700

    Consolidated 2 schemas into one for editing mechanics and parts on a service ticket.

--- app/blueprints/service_tickets/schemas.py
+++ app/blueprints/service_tickets/schemas.py
@@ -11,18 +11,13 @@ class ServiceTicketSchema(ma.SQLAlchemyAutoSchema): #TODO when I get the service
         fields = ("id", "mechanic_ids", "vin", "service_date", "service_desc", "customer_id", "mechanics", "customer", "parts")
 
 class EditServiceTicketSchema(ma.Schema):
-    add_mechanic_ids = fields.List(fields.Int(), required=True)
-    remove_mechanic_ids = fields.List(fields.Int(), required=True)
+    add_mechanic_ids = fields.List(fields.Int(), required=False)
+    remove_mechanic_ids = fields.List(fields.Int(), required=False)
+    add_part_id = fields.Int(required=False)
     class Meta:
-        fields = ("add_mechanic_ids", "remove_mechanic_ids")
-
-class AddPartServiceTicketSchema(ma.Schema):
-    add_part_id = fields.Int(required=True)
-    class Meta:
-        fields = ("add_part_id",)
+        fields = ("add_mechanic_ids", "remove_mechanic_ids", "add_part_id")
 
 service_ticket_schema = ServiceTicketSchema()
 service_tickets_schema = ServiceTicketSchema(many=True)
 return_service_ticket_schema = ServiceTicketSchema(exclude=["customer_id"])
 edit_service_ticket_schema = EditServiceTicketSchema()
-add_part_service_ticket_schema = AddPartServiceTicketSchema()
