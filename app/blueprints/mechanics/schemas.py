baf1e72af1fd5231254fd4c33c31d38106cd5b2e
joshuaharris07 <joshua.harris07@gmail.com>
2025-03-08 22:52:53 -0700

    Added ticket_count to show how many tickets are assigned to the mechanics

--- app/blueprints/mechanics/schemas.py
+++ app/blueprints/mechanics/schemas.py
@@ -1,9 +1,12 @@
+from marshmallow import fields
 from app.models import Mechanic
 from app.extensions import ma
 
 class MechanicSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Mechanic
+    
+    ticket_count = fields.Integer()
 
 mechanic_schema = MechanicSchema()
 mechanics_schema = MechanicSchema(many=True)
