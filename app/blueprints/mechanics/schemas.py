c2fc731944588dcd112040399651a32bb7e7f9d7
joshuaharris07 <joshua.harris07@gmail.com>
2025-03-11 06:59:05 -0600

    Added #of service-tickets assigned to a mechanic to the schema.

--- app/blueprints/mechanics/schemas.py
+++ app/blueprints/mechanics/schemas.py
@@ -1,12 +1,17 @@
 from marshmallow import fields
-from app.models import Mechanic
+from sqlalchemy import func
+from app.models import Mechanic, db
 from app.extensions import ma
 
 class MechanicSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Mechanic
     
-    ticket_count = fields.Integer()
+    ticket_count = fields.Method("get_ticket_count")
+
+    def get_ticket_count(self, mechanic):
+        count = db.session.query(func.count()).select_from(Mechanic).join(Mechanic.tickets).filter(Mechanic.id == mechanic.id).scalar()
+        return count or 0
 
 mechanic_schema = MechanicSchema()
 mechanics_schema = MechanicSchema(many=True)
